83da000cf6d9a9cb88b6225490725a7521f6bbb7
ErnestNeller <ernest.neller@altiplaconsulting.com>
2024-07-26 20:46:50 +0200

    Establece timeout para la petición de health check.

--- cmd/healthcheck/main.go
+++ cmd/healthcheck/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"flag"
 	"net/http"
 	"os"
@@ -8,26 +9,34 @@ import (
 )
 
 func main() {
-	url := flag.String("url", "", "URL to check")
-	filePath := flag.String("file", "", "File path to check")
-	timeout := flag.Duration("timeout", time.Second*5, "Timeout for the request")
+	url := *flag.String("url", "", "URL to check")
+	filePath := *flag.String("file", "", "File path to check")
+	timeout := *flag.Duration("timeout", time.Second*5, "Timeout for the request")
 
 	flag.Parse()
 
-	if *url != "" {
-		client := &http.Client{Timeout: *timeout}
-		resp, err := client.Get(*url)
+	if url != "" {
+		ctx, cancel := context.WithTimeout(context.Background(), timeout)
+		defer cancel()
+		req, err := http.NewRequestWithContext(ctx, http.MethodGet, url, nil)
 		if err != nil {
 			os.Exit(1)
 		}
+
+		client := http.Client{}
+		resp, err := client.Do(req)
+		if err != nil {
+			os.Exit(1)
+		}
+
 		defer resp.Body.Close()
 		if resp.StatusCode != http.StatusOK {
 			os.Exit(1)
 		}
 	}
 
-	if *filePath != "" {
-		if _, err := os.Stat(*filePath); os.IsNotExist(err) {
+	if filePath != "" {
+		if _, err := os.Stat(filePath); os.IsNotExist(err) {
 			os.Exit(1)
 		}
 	}
